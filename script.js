06a4f8b4c1a40ef78d4768dd3f76238275c56d6f
Nick <Nick_II2000@mail.ru>
2022-08-09 08:52:27 +0600

    2-th commit

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const btnPhone = document.querySelector('#iphone'),
-      btnMacbook = document.querySelector('#macbook'),
-      images = document.querySelectorAll('img');
+    btnMacbook = document.querySelector('#macbook'),
+    images = document.querySelectorAll('img');
 
+const phoneAnimation = images[0].animate([
+    { transform: 'translateY(0)' },
+    { transform: 'translateY(100px)' },
+    { transform: 'translateY(-100px)' },
+    { transform: 'translateY(0)' }
+], {
+    duration: 3000,
+    iteration: Infinity,
+});
