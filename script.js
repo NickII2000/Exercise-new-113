9a2a8aeb5e8d6e2986d9f3ba44d051cc0f44218b
Nick <Nick_II2000@mail.ru>
2022-08-09 10:47:04 +0600

    8-th commit

--- script.js
+++ script.js
@@ -57,13 +57,22 @@ let phoneAnimation;
 btnPhone.addEventListener('click', () => {
     if (!phoneAnimation) {
         phoneAnimation = images[0].animate([
-            { transform: 'translateY(0)' },
-            { transform: 'translateY(75px)' },
-            { transform: 'translateY(100px)' },
-            { transform: 'translateY(0)' },
-            { transform: 'translateY(-75px)' },
-            { transform: 'translateY(-100px)' },
-            { transform: 'translateY(0)' }
+            {
+                transform: 'translateY(0) rotate(0deg)',
+                filter: 'opacity(100%)'
+            },
+            {
+                transform: 'translateY(100px) rotate(120deg)',
+                filter: 'opacity(50%)'
+            },
+            {
+                transform: 'translateY(-100px) rotate(240deg)',
+                filter: 'opacity(75%)'
+            },
+            {
+                transform: 'translateY(0) rotate(360deg)',
+                filter: 'opacity(100%)'
+            }
         ], {
             duration: 1000,
             iterations: Infinity
