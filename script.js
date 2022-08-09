a37130d3aafdee76e8be10d627f60cad7a25db77
Nick <Nick_II2000@mail.ru>
2022-08-09 09:40:41 +0600

    4-th commit

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ const btnPhone = document.querySelector('#iphone'),
     btnMacbook = document.querySelector('#macbook'),
     images = document.querySelectorAll('img');
 
-const phoneAnimation = images[0].animate([
+const phoneAnimation0 = images[0].animate([
     { transform: 'translateY(0)' },
     { transform: 'translateY(100px)' },
     { transform: 'translateY(-100px)' },
     { transform: 'translateY(0)' }
 ], {
-    duration: 3000,
-    iteration: Infinity
+    duration: 1000,
+    iterations: Infinity
+});
+
+const phoneAnimation1 = images[1].animate([
+    // keyframes
+    { transform: 'scaleX(1)' },
+    { transform: 'scaleX(-0,25)' },
+    { transform: 'scaleX(-0,5)' },
+    { transform: 'scaleX(-0,75)' },
+    { transform: 'scaleX(-1)' },
+    { transform: 'scaleX(-0,75)' },
+    { transform: 'scaleX(-0,5)' },
+    { transform: 'scaleX(-0,25)' },
+    { transform: 'scaleX(1)' }
+], {
+    // timing options
+    duration: 1000,
+    iterations: Infinity
 });
