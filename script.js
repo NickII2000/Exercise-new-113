93804420f91b3f7fa9ec02521c6fad73b1037ffb
Nick <Nick_II2000@mail.ru>
2022-08-10 10:32:06 +0600

    19-th commit

--- script.js
+++ script.js
@@ -259,3 +259,19 @@ console.log(addAll3(...[10, 20, 30]));
 
 const addAll = (...x) => x.reduce((sum, item) => sum + item, 0);
 console.log(addAll(...[10, 20, 30, 40, 50]));
+
+// Максимально короткое решение :)
+const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
+
+const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+
+
+
+//проверка второй фунции
+
+const add1 = function (a) { return a + 1 };
+const addAll3 = function (a, b, c) { return a + b + c };
+
+const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+
+console.log(composeWithArgs(add1, addAll3)(1, 2, 3));  //=> Вернет 7
