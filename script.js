cf30d20731d5fd2674506019021d9a7103bc06c9
Nick <Nick_II2000@mail.ru>
2022-08-10 19:20:27 +0600

    21-th commit

--- script.js
+++ script.js
@@ -288,3 +288,19 @@ const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(.
 
 const discount = composeWithArgs(normalizePrice, myDiscont1rub, divide100, multiply20);
 console.log(discount(200.0));
+
+// Ex. 114
+
+'use strict';
+
+console.log(1);
+
+setTimeout(() => {
+    console.log('timeout_2000');
+}, 2000);
+
+setTimeout(() => {
+    console.log('timeout_4000');
+}, 4000);
+
+console.log(2);
